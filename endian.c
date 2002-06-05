6907e31974e68066950cac3ff1b5748f13acd853
Chris Lightfoot <chris@ex-parrot.com>
2002-06-05 00:29:43 +0000

    ""

--- endian.c
+++ endian.c
@@ -7,7 +7,7 @@
  *
  */
 
-static const char rcsid[] = "$Id: endian.c,v 1.3 2002/06/05 00:28:57 chris Exp $";
+static const char rcsid[] = "$Id: endian.c,v 1.4 2002/06/05 00:29:43 chris Exp $";
 
 #include <stdio.h>
 #ifdef USE_SYS_INT_TYPES_H
@@ -19,10 +19,8 @@ static const char rcsid[] = "$Id: endian.c,v 1.3 2002/06/05 00:28:57 chris Exp $
 int main(void) {
 #if defined(LITTLE_ENDIAN) || defined(_LITTLE_ENDIAN)
     printf("-DDRIFTNET_LITTLE_ENDIAN\n");
-    return 0;
 #elif defined(BIG_ENDIAN) || defined(_BIG_ENDIAN)
     printf("-DDRIFTNET_BIG_ENDIAN\n");
-    return 0;
 #else
     uint32_t a = 0;
     *((uint8_t*)&a) = 0xff;
@@ -32,6 +30,6 @@ int main(void) {
         printf("-DDRIFTNET_LITTLE_ENDIAN\n");
     else
         return -1; /* don't know. */
-    return 0;
 #endif  /* endianness test */
+    return 0;
 }
