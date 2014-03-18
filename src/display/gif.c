f45c98fe27d61468299da28f22276658339c5b95
David Suárez <david.sephirot@gmail.com>
2014-03-18 21:37:09 +0100

    Add support for giflib >= 5

--- src/display/gif.c
+++ src/display/gif.c
@@ -24,7 +24,14 @@
  */
 int gif_load_hdr(img I) {
     GifFileType *g;
+
+/* GIFLIB_MAJOR is not defined until version 5 of libgif */
+#if defined GIFLIB_MAJOR && GIFLIB_MAJOR >= 5
+    g = I->us = DGifOpenFileHandle(fileno(I->fp), NULL);
+#else
     g = I->us = DGifOpenFileHandle(fileno(I->fp));
+#endif
+
     if (!I->us) {
         I->err = IE_HDRFORMAT;
         return 0;
