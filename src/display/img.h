95d73a0543e34f66414561e3b1b71835d90a8ceb
Bastian Germann <bage@debian.org>
2023-10-07 15:06:52 +0200

    Recover else clause in endianess if
    The else clause was included in the inner (GTK3) if clause in
    465a1ff68a (Add RGB byte ordering implementation in PEL related macros).

--- src/display/img.h
+++ src/display/img.h
@@ -69,6 +69,7 @@ typedef uint32_t pel;
 #   define GETB(p)             ((chan)(((p) & (pel)0xff000000) >> 24))
 #   define GETA(p)             ((chan)(((p) & (pel)0x000000ff)      ))
 #endif
+#else
 #   error "no endianness defined"
 #endif
 
